5bd3e893e0e6956299fb6c047406acc68b9fb235
minhazapon <minhajulabedinapon@gmail.com>
2024-05-17 03:26:21 -0700

    menu files

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 import Banner from "./HomeFiles/Banner";
 import Banner2 from "./HomeFiles/Banner2";
 import Call from "./HomeFiles/Call";
+import Chef from "./HomeFiles/Chef";
 import Form from "./HomeFiles/Form";
+import Menu from "./HomeFiles/Menu";
 import Order from "./HomeFiles/Order";
 
 
@@ -12,7 +14,9 @@ const Home = () => {
             <Banner></Banner>
             <Order></Order>
             <Banner2></Banner2>
+            <Menu></Menu>
             <Call></Call>
+            <Chef></Chef>
             <Form></Form>
 
 
---
+++ src/HomeFiles/Menu.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+
+
+const Menu = () => {
+
+     
+     const [ menu, setMenu ] = useState([]);
+
+     useEffect( () => {
+
+        fetch('https://mocki.io/v1/0d51f391-fa10-43e6-813d-b6e1d568749f')
+        .then(res => res.json())
+        .then(data => {
+            
+            const popular = data.filter( item => item.category === 'popular' )
+            
+            setMenu(popular)})
+
+     } , [])
+
+
+
+
+    return (
+        <div className=" mb-10 mt-10 ml-10 mr-10">
+
+            <div>
+
+               <div>
+                    
+
+               <div className=" flex-col md:flex-row lg:flex-row p-10 text-center">
+                    <h1 className=" text-xl text-yellow-600">---Check it out---</h1>
+                    
+                    <div className=" flex justify-center mt-5">
+                    <p className=" bg-black w-[400px] h-[1px] text-center "></p>
+                    </div>
+
+                    <p className=" text-black text-5xl mt-3">FROM OUR MENU</p>
+
+                    <div className=" flex justify-center mt-5">
+                    <p className=" bg-black w-[400px] h-[1px] text-center "></p>
+                    </div>
+                     
+                </div>
+
+               </div>
+
+
+
+{/* ----------------------------------------------------------------------- */}
+             
+              <div>
+
+             
+
+
+
+
+              </div>
+
+
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Menu;
